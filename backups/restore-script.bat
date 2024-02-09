6b94cb2681877853a1ef26820a7145f82751c062
HiddenMW <qweewqqwe1236@gmail.com>
2024-02-10 00:47:17 +0800

    Server_Archive_2024/02/10  0:47:17.83

---
+++ backups/restore-script.bat
@@ -0,0 +1,4 @@
+@echo off 
+D:
+cd "D:\Enigmatica9Server-1.18.0\mods"
+java -jar "AdvancedBackups-forge-1.19.2-3.4.jar"
---
+++ backups/restore-script.sh
@@ -0,0 +1,2 @@
+cd "D:\Enigmatica9Server-1.18.0\mods"
+java -jar "AdvancedBackups-forge-1.19.2-3.4.jar"
